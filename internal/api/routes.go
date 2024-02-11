9f1159c7e52a478621fb31c2302661fc571619d1
Brandon Purvis <bpurvis@healthcheck360.com>
2024-02-10 22:41:12 -0600

    introduce openapi

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,14 +2,27 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "go-ledger/docs"
 )
 
 // RegisterRoutes registers all the routes for your application.
 func RegisterRoutes(r *gin.Engine) {
+	// Serve Swagger if available (make sure not to include it in production build)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.GET("/ping", pingHandler)
 }
 
-// pingHandler is an example handler for the /ping route.
+// pingHandler returns a pong response
+// @Summary Pong
+// @Description get a pong response
+// @Tags example
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} map[string]string
+// @Router /ping [get]
 func pingHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",

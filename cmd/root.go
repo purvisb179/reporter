9c10dd80c67f53c93503e2b9f655664eaacf89ed
Brandon Purvis <bpurvis@healthcheck360.com>
2024-02-10 20:59:32 -0600

    better error checking on viper init

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 func Execute() {
 	cobra.OnInitialize(initConfig)
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		_, _ = fmt.Fprintln(os.Stderr, err) // Explicitly ignoring the error
 		os.Exit(1)
 	}
 }
@@ -20,19 +20,24 @@ func initConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("json")
 	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("GL") // Prefix for environment variables
 	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
-	log.Printf("starting")
-	if err == nil {
-		log.Printf("using config.json file found on disk")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Printf("error reading config file: %v", err)
+		}
+		log.Println("starting without config.json file")
+	} else {
+		log.Println("using config.json file found on disk")
 	}
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "gl",
 	Short: "go-ledger",
+	Long:  `go-ledger is a CLI application for managing transactions.`,
 }
 
 func init() {
-	rootCmd.AddCommand()
+
 }

bb3978a6daa8e8fdc384e1918b360f3cffa17c3c
Brandon Purvis <bpurvis@healthcheck360.com>
2024-02-24 18:03:05 -0600

    serve html

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -4,24 +4,34 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
 	_ "reporter/docs"
 	"reporter/internal/service"
 )
 
-// RegisterRoutes registers all the routes for your application.
 func RegisterRoutes(r *gin.Engine, oidcService *service.OIDCService) {
-	// Serve Swagger if available (make sure not to include it in production build)
+	// Load HTML templates
+	r.LoadHTMLGlob("internal/templates/*")
+
+	// Swagger route
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	// Apply the OIDC authentication middleware to routes that require authentication
+	// OIDC authentication
 	authGroup := r.Group("/")
-
 	authGroup.Use(OIDCAuthMiddleware(oidcService))
 	{
 		authGroup.GET("/ping", pingHandler)
 	}
 
+	// Callback handler
 	r.GET("/callback", func(c *gin.Context) {
 		callbackHandler(c, oidcService)
+	})
+
+	// Serve HTML route
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "layout.html", gin.H{
+			"Title": "Home Page",
+		})
 	})
 }
